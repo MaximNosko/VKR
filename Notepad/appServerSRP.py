c98f22693c4ee1d201b774e75ad2fac62e8d362f
Максим <maxnosko@mail.ru>
2021-05-05 02:47:12 +0300

    Добавлены комментарии

--- Notepad/appServerSRP.py
+++ Notepad/appServerSRP.py
@@ -2,6 +2,6 @@ from base64 import b64encode
 from hashlib import sha256
 from hmac import HMAC
 
-def signSRP(secret_key,commandHash):
-    hash_code = b64encode(HMAC(secret_key.encode(), commandHash.encode(), sha256).digest())
-    return hash_code.decode('utf-8')
+def signSRP(secret_key,commandHash):# вычисление подписи по ключу и хешу команды
+    hash_code = b64encode(HMAC(secret_key.encode(), commandHash.encode(), sha256).digest())# вычисление подписи
+    return hash_code.decode('utf-8')#кодирование подписи в строку
--- Notepad/templates/appSRP.js
+++ Notepad/templates/appSRP.js
@@ -1,34 +1,44 @@
 let CLIENT_ADDRESS="http://localhost:49053/";
 async function runSRPCommand(appID,type,args,signURL,onWaiting,onDone,onError,onReturn)
 {
-	onWaiting();
-	args.path=encodeURIComponent(args.path.replaceAll("\\", "/"));
-	let commandObject={};
-	function getReadCommandObject(args,APP_ID)
+	//функция для отправки команды Клиенту СРП
+    //appID - идентификатор приложения
+    //type - тип команды
+    //args - объект, содержащий в себе аргументы команды
+    //signURL - адрес, по которому можно осуществить подпись хеша команды
+	//onWaiting - функция, которая будет вызвана во время ожидания завершения команды, аргументы не передаются
+	//onDone - функция, которая будет вызвана при успешном завершении команды, аргументы не передаются
+	//onError - функция, которая будет вызвана при неуспешном завершении команды, аргументы не передаются
+	//onReturn - функция, которая будет вызвана при успешном завершении, в качестве аргументов передаются объект результата выполнения команды и объект команды
+	onWaiting();//ожидание выполнения команды
+	args.path=encodeURIComponent(args.path.replaceAll("\\", "/"));//замена в пути всех обратных слешей на обычные
+	let commandObject={};//создание объекта команды
+	function getReadCommandObject(args,APP_ID)//функция для создания объекта команды на чтение
 	{
 		return {"type":"read","args":{"path":args.path},"appID":APP_ID};
 	}
-	function getWriteCommandObject(args,APP_ID)
+	function getWriteCommandObject(args,APP_ID)//функция для создания объекта команды на запись
 	{
 		return {"type":"write","args":{"path":args.path,"bytes":args.bytes},"appID":APP_ID};
 	}
-	function getListCommandObject(args,APP_ID)
+	function getListCommandObject(args,APP_ID)//функция для создания объекта команды на получение списка файлов
 	{
 		return {"type":"list","args":{"path":args.path},"appID":APP_ID};
 	}
-	function getDeleteCommandObject(args,APP_ID)
+	function getDeleteCommandObject(args,APP_ID)//функция для создания объекта команды на удаление
 	{
 		return {"type":"delete","args":{"path":args.path},"appID":APP_ID};
 	}
-	function getMkDirCommandObject(args,APP_ID)
+	function getMkDirCommandObject(args,APP_ID)//функция для создания объекта команды на создание папки
 	{
 		return {"type":"mkdir","args":{"path":args.path},"appID":APP_ID};
 	}
-	function getRenameCommandObject(args,APP_ID)
+	function getRenameCommandObject(args,APP_ID)//функция для создания объекта команды на переименование
 	{
 		args.newName=encodeURIComponent(args.newName);
 		return {"type":"rename","args":{"path":args.path,"newName":args.newName},"appID":APP_ID};
 	}
+	//перебор значений type c вызовом нужных функций
 	if(type==="read")
 	{
 		commandObject=getReadCommandObject(args,appID);
@@ -53,7 +63,7 @@ async function runSRPCommand(appID,type,args,signURL,onWaiting,onDone,onError,on
 	{
 		commandObject=getRenameCommandObject(args,appID);
 	}
-	function getSortedObject(uObj)
+	function getSortedObject(uObj)//фунекция для сортировки содержимого объекта команды
 	{
 		uObj["args"]=Object.keys(uObj["args"]).sort().reduce(
 		(obj, key) => { 
@@ -70,15 +80,15 @@ async function runSRPCommand(appID,type,args,signURL,onWaiting,onDone,onError,on
 		{}
 		);
 	}
-	commandObject=getSortedObject(commandObject);
-	async function sha256(message) {
+	commandObject=getSortedObject(commandObject);//сортировка объекта команты
+	async function sha256(message) {//функция для вычисления хеша SHA256
 		const msgBuffer = new TextEncoder().encode(message);                    
 		const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
 		const hashArray = Array.from(new Uint8Array(hashBuffer));            
 		const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 		return hashHex;
 	}
-	async function getHash(commandObject)
+	async function getHash(commandObject)//функция для вычисления хеша SHA256 для объекта команды
 	{
 		console.log("json");
 		console.log(JSON.stringify(commandObject))
@@ -87,8 +97,8 @@ async function runSRPCommand(appID,type,args,signURL,onWaiting,onDone,onError,on
 		console.log(t)
 		return t;
 	}
-	let commandHash=await getHash(commandObject);
-	function sendServerPost(hash,commandObject)
+	let commandHash=await getHash(commandObject);//вычисление хеша объекта команды
+	function sendServerPost(hash,commandObject)//функция для отправки на сервер веб-приложения хеша на подпись
 	{
 		var xhr = new XMLHttpRequest();
 		var url = signURL;
@@ -96,22 +106,22 @@ async function runSRPCommand(appID,type,args,signURL,onWaiting,onDone,onError,on
 		xhr.setRequestHeader("Content-Type", "application/json");
 		xhr.onreadystatechange = function () {
 			if (xhr.readyState === 4 && xhr.status === 200) {
-				receiveSign(xhr.responseText,commandObject);
+				receiveSign(xhr.responseText,commandObject);//вызов функции получения подписи
 			}
 		};
 		var data = JSON.stringify({"commandHash": hash});
 		xhr.send(data);
 	}
-	sendServerPost(commandHash,commandObject);
-	function receiveSign(sign,commandObject)
+	sendServerPost(commandHash,commandObject);//отправка хеша на подпись
+	function receiveSign(sign,commandObject)//функция для получения подписи
 	{
 		console.log("Получена подпись от сервера")
 		console.log(sign)
-		commandObject.serverSign=sign
+		commandObject.serverSign=sign//добавление подписи в объект команды
 		console.log(commandObject)
-		sendClientPost(commandObject);
+		sendClientPost(commandObject);//отправка команды Клиенту СРП
 	}
-	function sendClientPost(commandObject)
+	function sendClientPost(commandObject)//функция для отправки команды
 	{
 		console.log("Отправляем на Клиент:");
 		console.log(commandObject);
@@ -122,25 +132,33 @@ async function runSRPCommand(appID,type,args,signURL,onWaiting,onDone,onError,on
 		xhr.setRequestHeader("Content-Type", "application/json");
 		xhr.onreadystatechange = function () {
 			if (xhr.readyState === 4 && xhr.status === 200) {
-				receiveDataFromClient(xhr.responseText,commandObject)
+				receiveDataFromClient(xhr.responseText,commandObject)//вызов функции получения данных от Клиента СРП
 			}
 		};
 		var data = JSON.stringify(commandObject);
 		xhr.send(data);
 	}
-	function receiveDataFromClient(d,commandObject)
+	function receiveDataFromClient(d,commandObject)//функции для получения данных от Клиента СРП
 	{
 		console.log("Получен ответ от клиента");
 		console.log(d);
 		let responseObject=JSON.parse(d);
-		if(responseObject.result.success==="True")
+		if(responseObject.auth==="True")
 		{
-			onDone();
-			onReturn(responseObject,commandObject);
+			if(responseObject.result.success==="True")
+			{
+				onDone();//команда завершилась удачно
+				onReturn(responseObject,commandObject);//сообщение результата успешного выполнения команды веб-приложению
+			}
+			else
+			{
+				onError();//команда была завершена неудачно
+			}
 		}
 		else
 		{
-			onError();
+			onError();//команда была завершена неудачно
 		}
+		
 	}
 }
